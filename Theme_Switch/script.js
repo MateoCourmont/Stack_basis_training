031ee9b11e522515a06feb84c02977fbb504da35
Mateo Courmont <145758180+MateoCourmont@users.noreply.github.com>
2025-02-13 01:55:24 +0100

    commit - Theme Switch - button changes

--- Theme_Switch/script.js
+++ Theme_Switch/script.js
@@ -1,13 +1,19 @@
-document.getElementById("darkModeToggle").addEventListener("change", function() {
+document.getElementById("themeToggle").addEventListener("click", function() {
     var right = document.getElementById("right-div");
     var left = document.getElementById("left-div");
     right.classList.toggle("dark-mode");
     left.classList.toggle("dark-mode");
 
+    var themeIcon = document.getElementById("themeIcon");
     var statusText = document.getElementById("statusText");
-    if (this.checked) {
+
+    if (right.classList.contains("dark-mode")) {
+        themeIcon.src = "img/soleil.png";
+        themeIcon.alt = "Mode Sombre";
         statusText.textContent = "Toggle Light Mode";
     } else {
+        themeIcon.src = "img/lune.png";
+        themeIcon.alt = "Mode Clair";
         statusText.textContent = "Toggle Dark Mode";
     }
  })
